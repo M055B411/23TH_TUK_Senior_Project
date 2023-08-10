a93b59a597106b1f0dd7a59300538d34e925aaea
yunbem <yunbeomgo@gmail.com>
2023-08-10 19:00:24 +0900

    3인칭-1인칭 시점 변환 추가
    'c' 버튼 입력

--- Slime_Project/Source/Slime_Project/Slime_ProjectCharacter.h
+++ Slime_Project/Source/Slime_Project/Slime_ProjectCharacter.h
@@ -12,14 +12,6 @@ UCLASS(config=Game)
 class ASlime_ProjectCharacter : public ACharacter
 {
 	GENERATED_BODY()
-
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
 	
 	/** MappingContext */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
@@ -57,10 +49,5 @@ protected:
 	// To add mapping context
 	virtual void BeginPlay();
 
-public:
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
 
