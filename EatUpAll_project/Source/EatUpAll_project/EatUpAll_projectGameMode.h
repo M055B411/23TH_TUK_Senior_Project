d79ebd4dd2f32eaf91ab79cc24fab08b04b480bc
yunbem <yunbeomgo@gmail.com>
2023-10-28 13:57:42 +0900

    EatUpAll_project 업로드

---
+++ EatUpAll_project/Source/EatUpAll_project/EatUpAll_projectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EatUpAll_projectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEatUpAll_projectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEatUpAll_projectGameMode();
+};
+
+
+
