8803baf423a7ade75bc69841c5c4cd4c0304f9b8
yunbem <62534971+yunbem@users.noreply.github.com>
2024-01-08 17:19:29 +0900

    'EatUpAll' 언리얼 프로젝트 파일 업로드
    이전의 'EatUpAll_projcet' 언리얼 프로젝트의 플러그인 의존성을 제거하고 폴더명을 수정한 'EatUpAll' 언리얼 프로젝트를 업로드함. 이전의 'EatUpAll_project'는 지우지 않고 남겨두었음. (추후에는 지워야 함)

---
+++ EatUpAll/Source/EatUpAll/EatUpAll.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EatUpAll.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EatUpAll, "EatUpAll" );
+ 
