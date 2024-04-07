8448cb14d2dbf0eeb06770b4e25f97bc7ccaea04
Yang Jae Sung <imossball@gmail.com>
2024-04-07 22:36:59 +0900

    Edited BP_HunterPlayer
    -일단 팔 다시 추가함
    - 컨트롤러 추가함 (별 기능없고 인풋만 옮김)

---
+++ EatUpAll/Source/EatUpAll/Hunter_Controller.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hunter_Controller.h"
+
+void AHunter_Controller::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+}
+
+void AHunter_Controller::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ EatUpAll/Source/EatUpAll/Hunter_Controller.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Hunter_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EATUPALL_API AHunter_Controller : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void SetupInputComponent() override;
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input, meta = (AllowPrivateAccess = "true"))
+		class UInputMappingContext* MappingContext;
+};
