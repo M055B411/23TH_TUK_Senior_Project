434ba7b40206e7ec170cdb4760aa9a29843f90d2
yunbem <62534971+yunbem@users.noreply.github.com>
2024-04-30 14:22:50 +0900

    Feat: Add Epic Online Services login feature
    - Setup Epic Online Services Plugin, Project Setting
    - Add login function for Epic Online Services sessions

---
+++ EatUpAll/Source/EatUpAll/EOS_GameInstance.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EOS_GameInstance.h"
+#include "OnlineSubsystemUtils.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineIdentityInterface.h"
+
+void UEOS_GameInstance::LoginWithEOS(FString ID, FString Token, FString LoginType)
+{
+	IOnlineSubsystem* SubsystemRef = Online::GetSubsystem(this->GetWorld());
+	if (SubsystemRef)
+	{
+		IOnlineIdentityPtr IdentityPointerRef = SubsystemRef->GetIdentityInterface();
+		if (IdentityPointerRef)
+		{
+			FOnlineAccountCredentials AccountDetails;
+			AccountDetails.Id = ID;
+			AccountDetails.Token = Token;
+			AccountDetails.Type = LoginType;
+			IdentityPointerRef->OnLoginCompleteDelegates->AddUObject(this, &UEOS_GameInstance::LoginWithEOS_Return);
+			IdentityPointerRef->Login(0, AccountDetails);
+		}
+	}
+}
+
+FString UEOS_GameInstance::GetPlayerUsername()
+{
+	IOnlineSubsystem* SubsystemRef = Online::GetSubsystem(this->GetWorld());
+	if (SubsystemRef)
+	{
+		IOnlineIdentityPtr IdentityPointerRef = SubsystemRef->GetIdentityInterface();
+		if (IdentityPointerRef)
+		{
+			if (IdentityPointerRef->GetLoginStatus(0) == ELoginStatus::LoggedIn)
+			{
+				return IdentityPointerRef->GetPlayerNickname(0);
+			}
+		}
+	}
+	return FString();
+
+}
+
+bool UEOS_GameInstance::IsPlayerLoggedIn()
+{
+	IOnlineSubsystem* SubsystemRef = Online::GetSubsystem(this->GetWorld());
+	if (SubsystemRef)
+	{
+		IOnlineIdentityPtr IdentityPointerRef = SubsystemRef->GetIdentityInterface();
+		if (IdentityPointerRef)
+		{
+			if (IdentityPointerRef->GetLoginStatus(0) == ELoginStatus::LoggedIn)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+void UEOS_GameInstance::LoginWithEOS_Return(int32 LocalUserNum, bool bWasSuccess, const FUniqueNetId& UserID, const FString& Error)
+{
+	if (bWasSuccess)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Login Success"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Login Fail Reson - %s"), *Error);
+	}
+}
---
+++ EatUpAll/Source/EatUpAll/EOS_GameInstance.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "EOS_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EATUPALL_API UEOS_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable, Category="EOS Functions")
+	void LoginWithEOS(FString ID, FString Token, FString LoginType);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "EOS Functions")
+	FString GetPlayerUsername();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "EOS Functions")
+	bool IsPlayerLoggedIn();
+
+	void LoginWithEOS_Return(int32 LocalUserNum, bool bWasSuccess, const FUniqueNetId& UserID, const FString& Error);
+};
